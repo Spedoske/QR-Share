21967c5104e5e8d4c88a6ee82e68bfadfaa027ce
Spedoske <leomundspedoske@gmail.com>
2024-03-13 23:34:34 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useRef, useState} from "react";
+import Webcam from "react-webcam";
+import {scan, ScanResult} from 'qr-scanner-wechat'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const interval_ms = 100;
+
+const App = () => {
+    const webcamRef = useRef<null | Webcam>(null);
+    const [currentQRCode, setCurrentQRCode] = useState('');
+    useEffect(() => {
+        const decode = async () => {
+            const screenshot = webcamRef.current?.getCanvas();
+            if (!screenshot) {
+                await new Promise(resolve => setTimeout(resolve, interval_ms)).then(decode);
+                return;
+            }
+            let result: undefined | ScanResult = undefined;
+            try {
+                const context = screenshot.getContext('2d');
+                if (!context) {
+                    alert('Failed to get 2d context');
+                    throw "__";
+                }
+                result = await scan(context.getImageData(0, 0, screenshot.width, screenshot.height));
+            } catch (e) {
+                console.log(e);
+            }
+            if (result?.text) {
+                setCurrentQRCode(result?.text);
+            }
+            await new Promise(resolve => setTimeout(resolve, interval_ms)).then(decode);
+        }
+        decode();
+    }, []);
+
+    useEffect(() => {
+        if(currentQRCode && currentQRCode !== ''){
+            console.log(currentQRCode);
+            fetch(`/code?${encodeURIComponent(currentQRCode)}`);
+        }
+        }, [currentQRCode]);
+    return (
+        <Webcam ref={webcamRef}/>
+    );
 }
 
 export default App;
